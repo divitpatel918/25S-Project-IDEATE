d66c1bf7cc529438d442b52199bdfcd852043d1d
Yash Patel <ytpatel3@gmail.com>
2025-04-13 16:23:48 -0400

    some nav changes

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -11,19 +11,19 @@ def HomeNav():
 
 
 def AboutPageNav():
-    st.sidebar.page_link("pages/30_About.py", label="About", icon="🧠")
+    st.sidebar.page_link("pages/30_About.py", label="About", icon="❓")
 
 
-#### ------------------------ Examples for Role of pol_strat_advisor ------------------------
-def PolStratAdvHomeNav():
+#### ------------------------ Role of exec ------------------------
+def ExecHomeNav():
     st.sidebar.page_link(
-        "pages/00_Pol_Strat_Home.py", label="Political Strategist Home", icon="👤"
+        "pages/00_Exec_Home.py", label="Executive Board Member Home", icon="🕴️"
     )
 
 
 def WorldBankVizNav():
     st.sidebar.page_link(
-        "pages/01_World_Bank_Viz.py", label="World Bank Visualization", icon="🏦"
+        "pages/01_World_Bank_Viz.py", label="World Bank Visualization"
     )
 
 
@@ -31,7 +31,7 @@ def MapDemoNav():
     st.sidebar.page_link("pages/02_Map_Demo.py", label="Map Demonstration", icon="🗺️")
 
 
-## ------------------------ Examples for Role of usaid_worker ------------------------
+## ------------------------ Role of usaid_worker ------------------------
 def ApiTestNav():
     st.sidebar.page_link("pages/12_API_Test.py", label="Test the API", icon="🛜")
 
