faace37694d549e812853544e95f77ef0edceaeb
Aditya <appathak46@gmail.com>
2025-04-15 19:32:09 -0400

    deleted space

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -214,6 +214,7 @@ def SideBarLinks(show_home=False):
     # Always show the About page at the bottom of the list of links
     AboutPageNav()
 
+
     if st.session_state["authenticated"]:
         # Always show a logout button if there is a logged in user
         if st.sidebar.button("Logout"):
